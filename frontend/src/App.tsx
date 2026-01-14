fca82d4f7cc0d5bdb680267d91e67bba7ee4e064
Axel Velasquez <yoloax11@gmail.com>
2026-01-13 22:20:27 -0800

    feat: replace homepage placeholder with VideoStudio component in routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { VideoStudio } from '@/components/studio/VideoStudio';
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<>Homepage</>} />
+        <Route path="/" element={<VideoStudio />} />
       </Routes>
     </Router>
   )
